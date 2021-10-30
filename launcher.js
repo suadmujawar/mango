2bbd19d78c8670631987d6510d48d33fa610a0dd
suadmujawar <74352463+suadmujawar@users.noreply.github.com>
2021-10-30 15:31:19 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,28 @@
+class launcher{
+    constructor(body,anchor){
+        var options={
+            bodyA:body,
+            pointB:anchor,
+            stiffness:0.004,
+            legnth:10
+        }
+        this.launcher=Constraint.create(options);
+        this.bodyA=body;
+        this.pointB=anchor;
+        World.add(world,this.launcher);
+    }
+    attach(body){
+        this.launcher.bodyA=body;
+    }
+    fly(){
+        this.launcher.bodyA=null;
+    }
+    display(){
+        if(this.launcher.bodyA){
+            var pointA=this.bodyA.position;
+            var pointB=this.pointB;
+            strokeWeight(2);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+}
